2ae3c4a54cff0951867020f2aa39642d25b28bcc
mertio <mert7000@gmail.com>
2020-09-25 13:47:26 +0300

    made progress with projects page

---
+++ blog/crypto/internals.pl
@@ -0,0 +1,10 @@
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
+# Associate internals original text with physical files.
+
+
+$key = q/fig:fig1/;
+$ref_files{$key} = "$dir".q|main.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ blog/crypto/labels.pl
@@ -0,0 +1,17 @@
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
+# Associate labels original text with physical files.
+
+
+$key = q/fig:fig1/;
+$external_labels{$key} = "$URL/" . q|main.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
+# labels from external_latex_labels array.
+
+
+1;
+
